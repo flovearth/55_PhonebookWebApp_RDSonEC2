28f527488b62d12aedaed9cccac0894ec8fbcc39
fLoveARTh <flovearth@protonmail.com>
2020-10-21 19:45:36 +0200

    SG updated

--- phonebook-app.py
+++ phonebook-app.py
@@ -190,6 +190,6 @@ def delete_record():
 
 # Add a statement to run the Flask application which can be reached from any host on port 80.
 if __name__== '__main__':
-    # init_phonebook_db()
+    init_phonebook_db()
     # app.run(debug=True)
     app.run(host='0.0.0.0', port=80) 
